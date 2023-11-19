94e27eda0e6dcbf28db171ad0e0d26e689c78fc6
Kyryl Fesiuk <kirill.fesiuk@gmail.com>
2023-11-19 20:24:25 +0100

    Initial commit

---
+++ example.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    int x = 1;
+    float f;
+    int a = 3;
+    int y = 2232;
+    float f;
+    a = x * 3 + 5;
+    if (x > a)
+    {
+        if (x > a)
+        {
+            printf("test!");
+            a = x - 1;
+        }
+    }
+    else
+    {
+        x = a * 3 + 100;
+    }
+
+    if (x > 5756464)
+    {
+        for (int k = 0; k < 10; k++)
+        {
+            y = x + 3;
+        }
+    }
+}
